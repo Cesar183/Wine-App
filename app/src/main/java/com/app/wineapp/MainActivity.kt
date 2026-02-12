fc92f689f63bc782ca5f38e8be7b68afc7b0f51b
Cesar Martinez <cesar_a_mar_@hotmail.com>
2026-02-12 11:39:32 -0500

    Creacion de lista local

--- app/src/main/java/com/app/wineapp/MainActivity.kt
+++ app/src/main/java/com/app/wineapp/MainActivity.kt
@@ -33,9 +33,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun getWine(){
         val wines = getLocalWines()
+        adapter.submitList(wines)
     }
 
-    private fun getLocalWines() {
-        TODO("Not yet implemented")
+    private fun getLocalWines() = listOf(
+        Wine("Cursos", "Uva", Rating("4.8","236 ratings"),"Colombia", "https://images.vivino.com/thumbs/GpcSXs2ERS6niDxoAsvESA_pb_x300.png", 1),
+        Wine("Castilla", "Cereza", Rating("4.3","236 ratings"),"España", "https://images.vivino.com/thumbs/PBhGMcRNQ7aVnVNr7VgnWA_pb_x300.png", 2),
+        Wine("Casillero", "Manzana", Rating("4.2","236 ratings"),"Mexico", "https://images.vivino.com/thumbs/ZzMKzqFqRO-6oI3ys3gGgQ_pb_x300.png", 3),
+        Wine("Rivera", "Limon", Rating("4.5","236 ratings"),"Chile", "https://images.vivino.com/thumbs/easjTPIcS-mCQ99XoYOMgQ_pb_x300.png", 4),
+        Wine("California", "Lima", Rating("4.1","236 ratings"),"Argentina", "https://images.vivino.com/thumbs/U19RXtSdRMmoAesl2CBygA_pb_x300.png", 5)
+    )
+
+    override fun onResume() {
+        super.onResume()
+        getWine()
     }
 }
